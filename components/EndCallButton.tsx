48ca262075963ce1e2d569070e5827eb56a7be11
adityaaaaaaaa1407 <adityasagavekar14@gmail.com>
2024-04-14 11:55:48 +0530

    add endcall button component

---
+++ components/EndCallButton.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useCall, useCallStateHooks } from "@stream-io/video-react-sdk";
+import React from "react";
+
+const EndCallButton = () => {
+  const call = useCall();
+  const { useLocalParticipant } = useCallStateHooks();
+  const localParticipant = useLocalParticipant();
+  const isMeetingOwner = localParticipant && call?.state.createdBy  && localParticipant.userId === call?.state.createdBy.id;
+
+  return <div></div>;
+};
+
+export default EndCallButton;
--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,7 +1,93 @@
-import React from "react";
+import { cn } from "@/lib/utils";
+import {
+  CallControls,
+  CallParticipantsList,
+  CallStatsButton,
+  PaginatedGridLayout,
+  SpeakerLayout,
+} from "@stream-io/video-react-sdk";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
+import router, { useSearchParams } from "next/navigation";
+import React, { useState } from "react";
+import { LayoutList, Users } from "lucide-react";
+import { Button } from "./ui/button";
+import EndCallButton from "./EndCallButton";
+
+type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 const MeetingRoom = () => {
-  return <div>meeting room</div>;
+  const searchParams = useSearchParams();
+  const isPersonalRoom = !!searchParams.get("personalRoom");
+  // const router = useRouter()
+  const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
+  const [showParticipants, setShowParticipants] = useState(false);
+
+  const CallLayout = () => {
+    switch (layout) {
+      case "grid":
+        return <PaginatedGridLayout />;
+      case "speaker-right":
+        return <SpeakerLayout participantsBarPosition="left" />;
+      default:
+        return <SpeakerLayout participantsBarPosition="right" />;
+    }
+  };
+  return (
+    <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+      <div className="relative flex size-full items-center justify-center">
+        <div className="flex size-full max-w-[1000px] items-center">
+          <CallLayout />
+        </div>
+        <div
+          className={cn("h-[calc(100vh-86px)] hidden ml-2", {
+            "show-block": showParticipants,
+          })}
+        >
+          <CallParticipantsList onClose={() => setShowParticipants(false)} />
+        </div>
+      </div>
+      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
+        <CallControls />
+        <DropdownMenu>
+          <div className="flex items-center">
+            <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+              <LayoutList size={20} className="text-white" />
+            </DropdownMenuTrigger>
+          </div>
+          <DropdownMenuContent className="text-white border-dark-1 bg-dark-1">
+            {["grid", "speaker-left", "speaker-right"].map((item, index) => (
+              <div key={index}>
+                <DropdownMenuItem
+                  className="cursor-pointer"
+                  onClick={() =>
+                    setLayout(item.toLowerCase() as CallLayoutType)
+                  }
+                >
+                  {item}
+                </DropdownMenuItem>
+                <DropdownMenuSeparator className="border-dark-1" />
+              </div>
+            ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <CallStatsButton />
+        <Button onClick={() => setShowParticipants((prev) => !prev)}>
+          <div className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+            <Users size={20} className="text-white" />
+          </div>
+        </Button>
+        {!isPersonalRoom && (
+          <EndCallButton/>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default MeetingRoom;
