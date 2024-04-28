c70db2200f8be0491599d357dac8467273739d80
adityaaaaaaaa1407 <adityasagavekar14@gmail.com>
2024-04-28 10:58:21 +0530

    add previous section and complete it

--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 "use client";
 
 import { useGetCalls } from "@/hooks/useGetCalls";
@@ -5,6 +7,7 @@ import { Call, CallRecording } from "@stream-io/video-react-sdk";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import MeetingCard from "./MeetingCard";
+import Loader from "./Loader";
 
 const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
   const { endedCalls, upcomingCalls, callRecordings, isLoading } =
@@ -39,10 +42,45 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
 
   const calls = getCalls();
   const noCallsMessage = getNoCallsMessage();
+
+  if (isLoading) return <Loader />;
+
   return (
     <div className="grid grid-cols-1 gap-4 xl:grid-cols-2">
       {calls && calls.length > 0 ? (
-        calls.map((meeting: Call | CallRecording) => <MeetingCard />)
+        calls.map((meeting: Call | CallRecording) => (
+          <MeetingCard
+            key={(meeting as Call).id}
+            icon={
+              type === "ended"
+                ? "/icons/previous.svg"
+                : type === "upcoming"
+                ? "/icons/upcoming.svg"
+                : "/icons/recordings.svg"
+            }
+            title={
+              (meeting as Call).state.custom.description.substring(0, 26) ||
+              "No description "
+            }
+            date={
+              meeting.state.startsAt.toLocaleString() ||
+              meeting.start_time.tolocaleDateString()
+            }
+            isPreviousMeeting={type === "ended"}
+            buttonIcon1={type === "recordings" ? "/icons/play.svg" : undefined}
+            handleClick={
+              type === "recordings"
+                ? () => router.push(`${meeting.url}`)
+                : () => router.push(`/meeting/${meeting.id}`)
+            }
+            link={
+              type === "recordings"
+                ? meeting.url
+                : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meeting.id}`
+            }
+            buttonText={type === "recordings" ? "Play" : "Start"}
+          />
+        ))
       ) : (
         <p>{noCallsMessage}</p>
       )}
