f08a272d8dc1e0c6ad96ab227563b18f3a710ca5
Aditya Sagavekar <adityasagavekar14@gmail.com>
2024-06-09 17:57:19 +0530

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -4,12 +4,7 @@ import React from "react";
 const Home = () => {
   const now = new Date();
  const time = now.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-  const date = new Intl.DateTimeFormat("en-US", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "2-digit",
-  }).format(now);
+ const date = (new Intl.DateTimeFormat('en-US', { dateStyle: 'full' })).format(now);
   return (
     <section className="flex size-full flex-col gap-10 text-white ">
       <div className="h-[300px] w-full rounded-[20px] bg-hero bg-cover">
