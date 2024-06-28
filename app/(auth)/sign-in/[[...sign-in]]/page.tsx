0dfc6613a54bc1c720b31a2a8685d8bc2f1fbec8
adityaaaaaaaa1407 <adityasagavekar14@gmail.com>
2024-06-28 12:02:07 +0530

    fixing errors

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,12 +1,11 @@
+import { SignIn } from "@clerk/nextjs";
 
-import { SignIn } from "@clerk/nextjs"
+const SignInPage = () => {
+  return (
+    <div className="flex h-screen flex-col max-w-full items-center justify-center">
+      <SignIn />
+    </div>
+  );
+};
 
-const SignInPage = ()=>{
-    return(
-         <div className ="flex h-screen w-full items-center justify-center" >
-            <SignIn/>
-         </div>     
-    )
-}
-
-export default SignInPage
+export default SignInPage;
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -2,7 +2,7 @@ import { SignUp } from "@clerk/nextjs";
 
 const SignUpPage = () => {
   return (
-    <div className="flex h-screen w-full  items-center justify-center m-20 ">
+    <div className="flex h-screen max-w-full  items-center justify-center m-20 ">
       <SignUp />
     </div>
   );
